b76541066a957b999f1830609636a69a2deac953
Francisco Javier Arias <fariasweb@gmail.com>
2013-10-17 18:44:54 +0200

    Pruebas de navegacion

--- www/app/app.js
+++ www/app/app.js
@@ -1,4 +1,35 @@
-$( document ).on( "pageinit", "#demo-page", function() {
+$(document).bind("mobileinit", function () {
+	$.mobile.ajaxEnabled = false;
+    $.mobile.linkBindingEnabled = false;
+    $.mobile.hashListeningEnabled = false;
+    $.mobile.pushStateEnabled = false;
+});
+
+
+$( document ).on("pageinit", "#index", function() {
+	
+	$.mobile.ajaxEnabled = false;
+    $.mobile.linkBindingEnabled = false;
+    $.mobile.hashListeningEnabled = false;
+    $.mobile.pushStateEnabled = false;
+	
+		$.mobile.ajaxEnabled = false;
+		$.mobile.linkBindingEnabled = false;
+		$.mobile.hashListeningEnabled = false;
+		$.mobile.pushStateEnabled = false;
+		$.mobile.changePage.defaults.changeHash = false;
+
+	
+	$(document).on("pagechange", function(a, b) {
+		console.log("PAGECHANGE", a,b);
+		return false;
+	});
+	
+	$( window ).on( "navigate", function( event, data ) {
+  		console.log("NAVEIGATE", data );
+	});
+	
+	
     $( document ).on( "swipeleft swiperight", "#demo-page", function( e ) {
         // We check if there is no open panel on the page because otherwise
         // a swipe to close the left panel would also open the right panel (and v.v.).

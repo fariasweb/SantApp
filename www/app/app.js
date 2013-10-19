3b52b3bb46addd47a3b44c63e67441dd501a75f0
Dani Ruiz <druiz@admira.com>
2013-10-19 13:57:48 +0200

    organizando estructura

--- www/app/app.js
+++ www/app/app.js
@@ -9,4 +9,53 @@ $( document ).on( "pageinit", "#demo-page", function() {
             }
         }
     });
+
 });
+
+$(document).ready(function(){
+
+    //TODO: Pendiente obtener las categorías y pintarlas mediante templates.
+    // Las categorías tendrán una clase específica para identificarlas, las fichas otra
+
+    // TODO: Selector que identifica Categorías
+    $('a').on("click", function(e){
+        e.preventDefault();
+
+        // Navegamos a la ruta y guardamos información del href
+        $.mobile.navigate($(this).attr('href'), {info: $(this).attr('href')});
+
+        // Obtener categorías mediante id categoría
+        getCardsByCategory($(this).attr('href'));
+    });
+
+
+    // TODO: Selector que identifica Fichas
+    $('a').on("click", function(e){
+        e.preventDefault();
+
+        // Navegamos a la ruta y guardamos información del href
+        $.mobile.navigate($(this).attr('href'), {info: $(this).attr('href')});
+
+        // Obtener categorías mediante id categoría
+        getCard($(this).attr('href'));
+    });
+
+    // Información que obtenemos del método navigate
+    $( window ).on( "navigate", function( event, data ) {
+      console.log(data.state);
+
+    });
+
+});
+
+
+// Esto estaría en un controlador aparte
+function getCardsByCategory(category){
+    // Obtenemos fichas de la categoría recibida y compilamos template
+
+}
+
+// Esto estaría en un controlador aparte
+function getCard(card){
+    // Obtenemos información de la ficha y compilamos template
+}

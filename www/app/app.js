7a73b692e52b13cc0aa73b637548e8ff045c64ab
Issyu <isidro.fuentesm@gmail.com>
2013-10-21 18:46:50 +0200

    Vistas: Arreglo del icono update y ver mas

--- www/app/app.js
+++ www/app/app.js
@@ -1,5 +1,5 @@
-$( document ).on( "pageinit", "#demo-page", function() {
-    $( document ).on( "swipeleft swiperight", "#demo-page", function( e ) {
+$( document ).on( "pageinit", "#index", function() {
+    $( document ).on( "swipeleft swiperight", "#index", function( e ) {
         // We check if there is no open panel on the page because otherwise
         // a swipe to close the left panel would also open the right panel (and v.v.).
         // We do this by checking the data that the framework stores on the page element (panel: open).

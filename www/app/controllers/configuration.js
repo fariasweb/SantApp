045c71dc333fcca65c1ed8f05ddda66ff393d654
Dani Ruiz <druiz@admira.com>
2013-11-05 20:21:25 +0100

    PRUEBAS

--- www/app/controllers/configuration.js
+++ www/app/controllers/configuration.js
@@ -4,5 +4,24 @@
 
 app.controllers.configuration = function() {
 	console.log("I am going to configuration...");	
+	
+	var config = $('#config'),
+		configContent = config.find('[data-role=content]'),
+		configHeader = config.find('[data-role=header]'),
+		strHeader = "Configuration";
+	
+	configHeader.find('h1').html(strHeader);
+	
 	$.mobile.changePage($('#config'), {changeHash:false});
-}
+	
+	var configData = {};
+	
+	// Generamos template
+	var configTemplate = app.views.config;
+	var renderedTemplate = Mustache.render(configTemplate, configData);
+	
+	configContent.html(renderedTemplate);
+	
+	// Page create
+	config.trigger('pagecreate');
+};
